047e129f5d0d9f23a1fc66fe2fbc7f49d8f4e692
Pasang-lama <lpasang182@gmail.com>
2024-06-23 07:22:19 +0545

    add new files

---
+++ js/custome.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".banner-slider").owlCarousel({
+        items: 1,
+        loop : true,
+        autoplay : true,
+        nav : true,
+        navText: ['<i class="fa-solid fa-arrow-up-long"></i>', '<i class="fa-solid fa-arrow-down-long"></i>']
+    });
+
+  });
