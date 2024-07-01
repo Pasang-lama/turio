4446d7cdde1cd2fdbdddb88e175bceca6a593a4e
Pasang-lama <lpasang182@gmail.com>
2024-07-01 07:40:52 +0545

    works on the best tour slider

--- js/custome.js
+++ js/custome.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // banner slider 
     $(".banner-slider").owlCarousel({
         items: 1,
         loop : true,
@@ -7,4 +8,11 @@ $(document).ready(function(){
         navText: ['<i class="fa-solid fa-arrow-up-long"></i>', '<i class="fa-solid fa-arrow-down-long"></i>']
     });
 
+// best tour slider 
+    $(".best-selling-tour-slider").owlCarousel({
+        loop : true,
+        items: 3,
+        nav : true,
+        navText: ['<i class="fa-solid fa-arrow-left-long"></i>', '<i class="fa-solid fa-arrow-right-long"></i>']
+    });
   });
