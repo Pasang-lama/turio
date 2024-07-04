5fb005ecfd0fb85b5182b9d4b9a38abb194d3d41
Pasang-lama <lpasang182@gmail.com>
2024-07-04 07:56:19 +0545

    works on the media query

--- js/custome.js
+++ js/custome.js
@@ -13,6 +13,23 @@ $(document).ready(function(){
         loop : true,
         items: 3,
         nav : true,
-        navText: ['<i class="fa-solid fa-arrow-left-long"></i>', '<i class="fa-solid fa-arrow-right-long"></i>']
+        margin: 50,
+        navText: ['<i class="fa-solid fa-arrow-left-long"></i>', '<i class="fa-solid fa-arrow-right-long"></i>'],
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items : 1,
+               
+            },
+            // breakpoint from 480 up
+            480 : {
+             items: 2,
+            },
+            // breakpoint from 768 up
+            768 : {
+             items: 3
+            }
+        }
+
     });
   });
